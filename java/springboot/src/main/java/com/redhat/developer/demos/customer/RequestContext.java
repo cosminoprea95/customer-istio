fb6d38898ea7f9cd71006bdbb67f16e18312d5a5
Cosmin-Constantin Oprea <cosmin.constantin.oprea@gmail.com>
2020-03-03 16:35:29 +0100

    Add logging

--- java/springboot/src/main/java/com/redhat/developer/demos/customer/RequestContext.java
+++ java/springboot/src/main/java/com/redhat/developer/demos/customer/RequestContext.java
@@ -10,21 +10,21 @@ public class RequestContext {
 
     public static final String REQUEST_HEADER_NAME = "x-api-key";
 
-//    private static final ThreadLocal<RequestContext> CONTEXT = new ThreadLocal<>();
+    private static final ThreadLocal<RequestContext> CONTEXT = new ThreadLocal<>();
 
     private String token;
 
     private HashMap<String, String> headers;
 
     public static RequestContext getContext() {
-//        RequestContext result = CONTEXT.get();
+        RequestContext result = CONTEXT.get();
 
-//        if (result == null) {
-//            result = new RequestContext();
-//            CONTEXT.set(result);
-//        }
+        if (result == null) {
+            result = new RequestContext();
+            CONTEXT.set(result);
+        }
 
-        return new RequestContext();
+        return result;
     }
 
     public HashMap<String, String> getHeaders() {
