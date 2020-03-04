1da3c9e605006eba9cf6f2c3ad5f148e335ee0de
Cosmin-Constantin Oprea <cosmin.constantin.oprea@gmail.com>
2020-03-04 11:54:03 +0100

    Add logging

--- java/springboot/src/main/java/com/redhat/developer/demos/customer/CustomerApplication.java
+++ java/springboot/src/main/java/com/redhat/developer/demos/customer/CustomerApplication.java
@@ -1,25 +1,14 @@
 package com.redhat.developer.demos.customer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.annotation.PostConstruct;
-import java.util.List;
 
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableScheduling
 public class CustomerApplication{
 
     public static void main(String[] args) {
